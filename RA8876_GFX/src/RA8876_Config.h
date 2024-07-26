9c75f397b49874beda2f3a8ef4bcf959eb235a31
Warren Watson <wwatson4506@gmail.com>
2024-07-25 19:02:00 -0700

    Add RA8876_Config.h file.

---
+++ RA8876_GFX/src/RA8876_Config.h
@@ -0,0 +1,14 @@
+/* RA8876_Config.h
+ A file to place user defines and configs.
+*/
+
+#ifndef RA8876_CONFIG_H
+#define RA8876_CONFIG_H
+
+// Uncomment to use SPI interface instead of 8080 interface.
+#define USE_SPI
+
+// Uncomment to use FT5206 touch.
+#define USE_FT5206_TOUCH
+
+#endif // RA8876_CONFIG_H
